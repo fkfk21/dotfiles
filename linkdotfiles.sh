1f423f0e669c235e167e353f775441dfb40f2a78
Kenji Fukuda <fk.kenji.21@gmail.com>
2020-04-11 02:55:53 +0900

    add: git-prompt to display branch name

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -2,3 +2,16 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -sf ~/dotfiles/.git-prompt-export ~/.git-prompt-export
+prom="source ~/.git-prompt-export"
+grep "$prom" ~/.bashrc
+if [ $? = 0 ]; then
+  echo "$prom :: exist in .bashrc"
+else
+  echo "$prom :: not exist in .bashrc"
+  echo $prom >> ~/.bashrc
+fi
+
+
